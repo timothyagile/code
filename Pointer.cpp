07bec7f5ccc057a29e004cf4352b6569ee7d7567
Thinh_dep_trai <113797076+timothyagile@users.noreply.github.com>
2022-12-16 22:42:18 +0700

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main ()
+{
+	//int a;
+	//int b;
+	//a = 10;
+	//con tro la mot bien dac biet, dung de luu gia tri cua bien khac.
+	//cu phap khai bao bien con tro: <kieu du lieu> *<ten bien>
+	//int *p;
+	//toan tu &: dung de lay dia chi cua 1 bien.
+	//p = &a;
+	//cout << p;
+	//toan tu *: dung de lay gia tri tai 1 dia chi bat ki
+	//cout << *p;
+	//*p = 20;
+	//-------------------
+	//cout << *p;
+	//cout << a;
+	//-------------------
+	//su khac biet cua bien con tro va bien thuong.
+	/*b = a;
+	a = a + 1;
+	cout << a << endl;
+	cout << b << endl; */
+	
+	//---------------
+	
+	/*//cap phat tinh
+	int a[100];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+	cin >> a[i];
+	}
+	*/
+	//cap phat dong
+	
+	/*int *t = new int();
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> t[i];
+	}
+	for (int i = 0; i < n; i++)
+	{
+		cout << t[i] << " ";
+	}*/
+	//delete t; 
+	
+	return 0;
+}
